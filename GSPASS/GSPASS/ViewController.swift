acf589d5b8c82fb74f233b15cb03980ea1f16c81
kimxwan0319 <kimxwan0319@naver.com>
2021-05-20 18:49:42 +0900

    ⚙️ :: 프로젝트 생성

---
+++ GSPASS/GSPASS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GSPASS
+//
+//  Created by 김수완 on 2021/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
