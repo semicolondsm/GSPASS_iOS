e203c84bc63736409cb63fd70ec2caa34a089a22
kimxwan0319 <kimxwan0319@naver.com>
2021-05-26 14:50:50 +0900

    ⚡️ :: GSPASSAPI 틀 잡기

--- GSPASS/GSPASS/Model/Network/GSPASSAPI.swift
+++ GSPASS/GSPASS/Model/Network/GSPASSAPI.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+import Alamofire
+import KeychainSwift
+
+enum GSPASSAPI {
+    
+}
+
+extension GSPASSAPI {
+    
+    public var url: String {
+        
+        let baseUrl = ""
+        
+        return baseUrl + path
+    }
+    
+    public var method: HTTPMethod {
+        switch self {
+        }
+    }
+    
+    public var parameters: Parameters? {
+        switch self {
+        }
+    }
+    
+    public var encoding: ParameterEncoding {
+        switch self.method {
+        case .get:
+            return URLEncoding.queryString
+        default:
+            return JSONEncoding.default
+        }
+    }
+    
+    public var header: HTTPHeader {
+        switch self {
+        }
+    }
+    
+    
+    private var path: String {
+        switch self {
+        }
+    }
+    
+    private var accessTocken: String {
+        let keychain = KeychainSwift()
+        return keychain.get("ACCESS-TOKEN") ?? ""
+    }
+    
+    private var refreshToken: String {
+        let keychain = KeychainSwift()
+        return keychain.get("REFRESH-TOKEN") ?? ""
+    }
+}
