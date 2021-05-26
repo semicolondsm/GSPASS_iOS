04da4a21b9f81a46c73d590c43179921d504162b
kimxwan0319 <kimxwan0319@naver.com>
2021-05-26 19:27:13 +0900

    ⚡️ :: StatusCode들 추가

--- GSPASS/GSPASS/Model/Network/StatusCode.swift
+++ GSPASS/GSPASS/Model/Network/StatusCode.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+enum StatusCode: Error {
+    case OK                  // 200; 성공
+    case badRequest          // 400; 올바르지 않은 요청
+    case unauthorized        // 401; 토큰 인증 실패
+    case forbidden           // 402; 권한 없음
+    case notFound            // 404; 찾을 수 없는 경로
+    case internalServerError // 500; 서버 에러
+}
