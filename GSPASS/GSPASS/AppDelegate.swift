37ccfc218abec6eec55f2169575931717fd3540f
kimxwan0319 <kimxwan0319@naver.com>
2021-05-31 10:10:54 +0900

    ⚡️ :: 토큰 재발급이 실패했을때 RootViewController를 로그인 페이지로 변경 구현

--- GSPASS/GSPASS/AppDelegate.swift
+++ GSPASS/GSPASS/AppDelegate.swift
@@ -6,17 +6,35 @@
 //
 
 import UIKit
+import RxSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let disposeBag = DisposeBag()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        HTTPClient.shared.networking(.tokenRefresh, TokenModel.self).subscribe(onNext: { token in
+            self.setRootViewController("Main", "MainViewController")
+        }, onError: { _ in
+            self.setRootViewController("Auth", "LoginViewController")
+        }).disposed(by: disposeBag)
+        
+        
+        
         return true
     }
-
+    
+    private func setRootViewController(_ storyboardName: String, _ viewControllerIdentifier: String){
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let tutorialStoryboard = UIStoryboard(name: storyboardName, bundle: nil)
+        let viewController = tutorialStoryboard.instantiateViewController(withIdentifier: viewControllerIdentifier)
+        self.window?.rootViewController = viewController
+        self.window?.makeKeyAndVisible()
+    }
 
 }
 
