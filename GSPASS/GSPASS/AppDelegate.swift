666e084065fc4db0947b3376fcbefb15fcb54fc6
kimxwan0319 <kimxwan0319@naver.com>
2021-05-31 10:14:48 +0900

    ⚡️ :: 토큰 재발급 성공시 키체인에 저장

--- GSPASS/GSPASS/AppDelegate.swift
+++ GSPASS/GSPASS/AppDelegate.swift
@@ -7,17 +7,21 @@
 
 import UIKit
 import RxSwift
+import KeychainSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     let disposeBag = DisposeBag()
+    let keychain = KeychainSwift()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         HTTPClient.shared.networking(.tokenRefresh, TokenModel.self).subscribe(onNext: { token in
+            self.keychain.set("ACCESS-TOKEN", forKey: token.access_token)
+            self.keychain.set("REFRESH-TOKEN", forKey: token.refresh_token)
             self.setRootViewController("Main", "MainViewController")
         }, onError: { _ in
             self.setRootViewController("Auth", "LoginViewController")
