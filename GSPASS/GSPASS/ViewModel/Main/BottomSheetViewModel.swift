acd41310619bf4dd37320a6c7fb8ff549527da58
kimxwan0319 <kimxwan0319@naver.com>
2021-06-07 23:39:22 +0900

    ⚡️ :: BottomSheetViewModel 생성

---
+++ GSPASS/GSPASS/ViewModel/Main/BottomSheetViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  BottomSheetViewModel.swift
+//  GSPASS
+//
+//  Created by 김수완 on 2021/06/07.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+class BottomSheetViewModel: ViewModel {
+
+    let disposeBag = DisposeBag()
+    
+    struct Input {
+        let issuePass: Driver<Void>
+    }
+
+    struct Output {
+        let resultOfIssuPass: Observable<String>
+    }
+
+    func transform(_ input: Input) -> Output {
+        let resultOfIssuPass = PublishSubject<String>()
+        
+        input.issuePass.asObservable().subscribe(onNext: {
+            HTTPClient.shared.networking(.issuePass, EmptyResponseModel.self).subscribe(onSuccess: { _ in
+                resultOfIssuPass.onCompleted()
+            }, onFailure: { error in
+                switch error as? StatusCode {
+                case .badRequest:
+                    resultOfIssuPass.onNext("신청 시간이 아닙니다!")
+                default:
+                    resultOfIssuPass.onNext("인터넷을 확인해주세요!")
+                }
+            })
+            .disposed(by: self.disposeBag)
+        })
+        .disposed(by: disposeBag)
+        
+        return Output(resultOfIssuPass: resultOfIssuPass)
+    }
+
+}
